9e27f1a18287c77f368d5be77548cbe8e861ecca
wesleynepo <wesleynepomuceno1@gmail.com>
2022-05-02 17:33:11 -0300

    feat: persists cookie for 365 days

--- src/utils/token/index.ts
+++ src/utils/token/index.ts
@@ -2,7 +2,7 @@ import Cookie from 'js-cookie'
 const tokenLocalKey = 'token-senior'
 
 export const setTokenCookie = (token: string) => {
-  Cookie.set(tokenLocalKey, token)
+  Cookie.set(tokenLocalKey, token, { expires: 365 })
 }
 
 export const getTokenCookie = () => {
