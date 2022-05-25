721c1cf84c7c75d1b20559927dee49748a10bda2
wesleynepo <wesleynepomuceno1@gmail.com>
2022-05-24 22:32:32 -0300

    feat: add react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { Template } from './components/Template'
 import { SeniorContextProvider } from './contexts/SeniorContext'
@@ -10,9 +11,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider>
       <SeniorContextProvider>
-        <Template>
-          <App />
-        </Template>
+        <BrowserRouter>
+          <Template>
+            <App />
+          </Template>
+        </BrowserRouter>
       </SeniorContextProvider>
     </ChakraProvider>
   </React.StrictMode>
