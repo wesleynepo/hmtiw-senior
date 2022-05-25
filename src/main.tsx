f56fa814e68f02c76dc9ba8d4eff46d6ba736244
wesleynepo <wesleynepomuceno1@gmail.com>
2022-05-24 22:50:32 -0300

    feat: create routes and add to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,25 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
 import { Template } from './components/Template'
 import { SeniorContextProvider } from './contexts/SeniorContext'
 import './index.css'
+import { History } from './routes/history'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider>
       <SeniorContextProvider>
-        <BrowserRouter>
-          <Template>
-            <App />
-          </Template>
-        </BrowserRouter>
+        <Template>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="/historico" element={<History />} />
+            </Routes>
+          </BrowserRouter>
+        </Template>
       </SeniorContextProvider>
     </ChakraProvider>
   </React.StrictMode>
