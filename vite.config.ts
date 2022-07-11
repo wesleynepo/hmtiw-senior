ec5d07ff6cd37593dd44c65611a67e79309253fe
CarolinaAguera <carolina.aguera@gazin.com.br>
2022-07-11 19:49:58 -0300

    fix: add mask images

--- vite.config.ts
+++ vite.config.ts
@@ -25,12 +25,14 @@ export default defineConfig({
           {
             src: '/src/icon.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: '/src/mask-icon.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
