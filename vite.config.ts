38f840977ebd04c4a1d9ac9edfddc21c5e2270f2
CarolAguera <carolaguerabr@gmail.com>
2022-07-11 08:43:32 -0300

    feat: add pwa in application

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'inline',
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: [
+        '/src/icon.svg',
+        '/src/apple-touch-icon.svg',
+        '/src/mask-icon.svg'
+      ],
+      manifest: {
+        name: 'HMTIW Senior',
+        short_name: 'HMTIW',
+        description: 'HMTIW Senior',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/src/icon.svg',
+            sizes: '192x192',
+            type: 'image/svg'
+          },
+          {
+            src: '/src/mask-icon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
