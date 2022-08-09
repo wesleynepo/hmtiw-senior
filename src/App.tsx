12672e5383c0dbd68d6c771fa3e52825f8ff3e28
wesleynepo <wesleynepomuceno1@gmail.com>
2022-08-08 23:30:43 -0300

    feat: the end

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,37 @@
-import { TokenForm } from './components/TokenForm'
-import { Center, Spinner, VStack } from '@chakra-ui/react'
-import { useSeniorContext } from './hooks/useSenior'
-import { WorkClock } from './components/WorkClock'
+import { Center, Text, VStack } from '@chakra-ui/react'
+import useSound from 'use-sound'
+import song from './mao.mp3'
 import { motion } from 'framer-motion'
 
 function App() {
-  const { token, loading } = useSeniorContext()
-
-  if (loading)
-    return (
-      <Center mt="50%">
-        <Spinner />
-      </Center>
-    )
-
-  if (token)
-    return (
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 0.1 }}
-        exit={{ opacity: 0 }}
-      >
-        <VStack spacing="100">
-          <WorkClock />
-        </VStack>
-      </motion.div>
-    )
-
+  const [play] = useSound(song, { volume: 0.3 })
+  play()
   return (
     <Center mt="10%">
-      <TokenForm />
+      <VStack>
+        {' '}
+        <Text fontWeight="bold" color="red" fontSize="2xl">
+          Deprecated
+        </Text>
+        <Text fontStyle="italic">
+          The project is open source you can host your own!
+        </Text>
+        <Text>
+          I hope you guys have a good day and I wish the best of luck for you!
+        </Text>
+        <motion.div
+          animate={{ rotate: 720 }}
+          transition={{
+            duration: 1.5,
+            repeat: Infinity,
+            repeatType: 'loop'
+          }}
+        >
+          <Text color="red" fontSize="5xl">
+            ★
+          </Text>
+        </motion.div>
+      </VStack>
     </Center>
   )
 }
--- src/components/Template/index.tsx
+++ src/components/Template/index.tsx
@@ -1,33 +1,9 @@
-import { Center, HStack, Text, VStack } from '@chakra-ui/react'
 import { ReactNode } from 'react'
-import { Logo } from '../Logo'
-import { ToggleButton } from '../ToggleButton'
-import { ForkMe } from 'fork-me-corner'
-import { Link } from 'react-router-dom'
 
 export type TemplateProps = {
   children?: ReactNode
 }
 
 export const Template = ({ children }: TemplateProps) => {
-  return (
-    <VStack>
-      <HStack
-        mt="1rem"
-        justifyContent="space-between"
-        width={{ base: '50%', md: '90%' }}
-      >
-        <HStack>
-          <Logo />
-          <Link to="/historico">
-            <Text>Histórico</Text>
-          </Link>
-        </HStack>
-
-        <ToggleButton />
-      </HStack>
-      <ForkMe repo="https://github.com/wesleynepo/hmtiw-senior" />
-      <Center width="full">{children}</Center>
-    </VStack>
-  )
+  return <>{children}</>
 }
